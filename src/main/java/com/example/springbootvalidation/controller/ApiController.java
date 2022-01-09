317e579f8952fa5ff3a2083c32d4a26543cb81d5
ahyoon99 <kimay9623@naver.com>
2022-01-09 16:19:24 +0900

    Spring Boot Validation 적용하기 전 코드

---
+++ src/main/java/com/example/springbootvalidation/controller/ApiController.java
@@ -0,0 +1,23 @@
+package com.example.springbootvalidation.controller;
+
+import com.example.springbootvalidation.dto.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/user")
+    public ResponseEntity user(@RequestBody User user){
+        System.out.println(user);
+        if (user.getAge()>=90){ // age가 90이상이면 에러를 던져준다.
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        }
+        return ResponseEntity.ok(user);
+    }
+}
