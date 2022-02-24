4ef42630912a49cdc8f333a937d6c3bb0ef160c8
ahyoon99 <kimay9623@naver.com>
2022-02-24 18:17:06 +0900

    AssertTrue 사용

--- src/main/java/com/example/springbootvalidation/dto/User.java
+++ src/main/java/com/example/springbootvalidation/dto/User.java
@@ -1,6 +1,8 @@
 package com.example.springbootvalidation.dto;
 
 import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class User {
 
@@ -15,6 +17,9 @@ public class User {
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸도폰 번호의 양식과 맞지 않습니다. 01x-xx(x)-xxxx")
     private String phoneNumber;
+
+    @Size(min = 6, max = 6)
+    private String reqYearMonth;    // yyyyMM
 
     public String getName() {
         return name;
@@ -48,6 +53,24 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    @AssertTrue(message = "yyyyMM의 형식에 맞지 않습니다.")
+    public boolean isReqYearMonthValidation(){  // @AssertTrue를 쓰기 위해서는 메소드 이름이 항상 is로 시작되어야 한다. @AssertTrue는 재사용이 불가능하다.
+        try{
+            LocalDate localDate = LocalDate.parse(getReqYearMonth()+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+        } catch(Exception e){
+            return false;
+        }
+        return true;    // return값이 true이면 정상인 것이다.
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -55,6 +78,7 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
     }
 }
