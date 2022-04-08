e916e9e812eb525e6fd14cf7585a30c64f32ec76
ahyoon99 <kimay9623@naver.com>
2022-04-08 15:15:47 +0900

    RestTemplate - getForEntity() 사용

--- src/main/java/com/example/client/service/RestTemplateService.java
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -27,11 +27,17 @@ public class RestTemplateService {
         // 이 uri를 restTemplate을 통해서 통신을 할 것이다.
         RestTemplate restTemplate = new RestTemplate();
         // 1. getForObject 사용 : 두번째 파라미터 값인 String.class 타입이 리턴된다.
-        String result = restTemplate.getForObject(uri, String.class);   // String.class 타입으로 리턴값을 받아보겠다.
-                                                                        // getForObject()가 실행되는 순간이 client에서 http 서버로 붙는 순간이다.
-                                                                        // getForObject에서의 get은 가져오겠다의 get이 아니라 http 메소드의 get을 의미한다.
-        return result;
+//        String result = restTemplate.getForObject(uri, String.class);   // String.class 타입으로 리턴값을 받아보겠다.
+//                                                                        // getForObject()가 실행되는 순간이 client에서 http 서버로 붙는 순간이다.
+//                                                                        // getForObject에서의 get은 가져오겠다의 get이 아니라 http 메소드의 get을 의미한다.
+//        return result;
 
+        //  2. getForEntity 사용 : ResponseEntity<T> 타입이 리턴된다.
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);   // result에 여러가지 정보가 담겨있다.
+        System.out.println(result.getStatusCode()); // http의 status code를 볼 수 있다. // 200 OK 가 출려된다.
+        System.out.println(result.getBody());   // response에 들어있는 내용이 들어있다. // hello server 가 출력된다.
+
+        return result.getBody();
     }
 }
 
