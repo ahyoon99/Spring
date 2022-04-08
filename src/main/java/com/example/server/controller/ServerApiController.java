0783c789e16584c537783bd00d6b6dcf93a0d68a
ahyoon99 <kimay9623@naver.com>
2022-04-08 15:21:21 +0900

    RestTemplate - getForEntity() 사용

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/server")
 public class ServerApiController {
 
+    // getForObject()든, getForEntity()든 server에서는 바뀌는 코드가 없다.
     @GetMapping("/hello")
     public String hello(){
         return "hello server";
