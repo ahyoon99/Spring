cbb833f61035ae057f7f06f3630bdca8b4986476
ahyoon99 <kimay9623@naver.com>
2022-04-08 15:32:50 +0900

    RestTemplate - json 형식으로 response 받기 +) RequestParameter사용하기

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,6 +3,7 @@ package com.example.server.controller;
 import com.example.server.dto.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +12,10 @@ public class ServerApiController {
 
     // getForObject()든, getForEntity()든 server에서는 바뀌는 코드가 없다.
     @GetMapping("/hello")
-    public User hello(){
+    public User hello(@RequestParam String name, @RequestParam int age){
         User user = new User();
-        user.setName("steve");
-        user.setAge(10);
+        user.setName(name);
+        user.setAge(age);
         return user;
     }
 }
