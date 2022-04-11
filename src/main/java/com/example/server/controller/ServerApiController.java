1d29749bba95b038a5a3dab389dcad0802e88471
ahyoon99 <kimay9623@naver.com>
2022-04-11 15:31:51 +0900

    Naver 지역 검색 API 연동

--- src/main/java/com/example/server/controller/ServerApiController.java
+++ src/main/java/com/example/server/controller/ServerApiController.java
@@ -4,7 +4,14 @@ import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
 
 @Slf4j
 @RestController
@@ -52,5 +59,40 @@ public class ServerApiController {
         response.setResBody(user.getResBody());
 
         return response;
+    }
+
+    // https://openapi.naver.com/v1/search/local.json
+    // ?query= %EA%B0%88%EB%B9%84%EC%A7%91
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver(){
+
+        String query = "갈비집";
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", query)
+                .queryParam("display",10)
+                .queryParam("start",1)
+                .queryParam("sort","random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        log.info("uri : {}", uri);
+
+        RestTemplate restTemplate = new RestTemplate();
+        // Header 넣기
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id","j6QFiAfdMdtTMDBQN6E8")
+                .header("X-Naver-Client-Secret","Qyz1Tnin3i")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+        return result.getBody();
     }
 }
