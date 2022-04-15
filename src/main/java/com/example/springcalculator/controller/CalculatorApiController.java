932a88a89ade41e51d4ee8aa8e3544197bbea9e0
ahyoon99 <kimay9623@naver.com>
2022-04-15 19:05:47 +0900

    CalculatorApiController POST Test

--- src/main/java/com/example/springcalculator/controller/CalculatorApiController.java
+++ src/main/java/com/example/springcalculator/controller/CalculatorApiController.java
@@ -2,10 +2,10 @@ package com.example.springcalculator.controller;
 
 import com.example.springcalculator.component.Calculator;
 import com.example.springcalculator.component.ICalculator;
+import com.example.springcalculator.dto.Req;
+import com.example.springcalculator.dto.Res;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -14,13 +14,24 @@ public class CalculatorApiController {
 
     private final Calculator calculator;
 
-    @RequestMapping("/sum")
+    @GetMapping("/sum")
     public int sum(@RequestParam int x, @RequestParam int y){
         return calculator.sum(x,y);
     }
 
-    @RequestMapping("/minus")
-    public int minus(@RequestParam int x, @RequestParam int y){
-        return calculator.minus(x,y);
+//    @GetMapping("/minus")
+//    public int minus(@RequestParam int x, @RequestParam int y){
+//        return calculator.minus(x,y);
+//    }
+
+    @PostMapping("/minus")
+    public Res minus(@RequestBody Req req){
+
+        int result = calculator.minus(req.getX(), req.getY());
+
+        Res res = new Res();
+        res.setResult(result);
+        res.setResponse(new Res.Body());
+        return res;
     }
 }
--- src/test/java/com/example/springcalculator/controller/CalculatorApiControllerTest.java
+++ src/test/java/com/example/springcalculator/controller/CalculatorApiControllerTest.java
@@ -3,14 +3,19 @@ package com.example.springcalculator.controller;
 import com.example.springcalculator.component.Calculator;
 import com.example.springcalculator.component.DollarCalculator;
 import com.example.springcalculator.component.MarketApi;
+import com.example.springcalculator.dto.Req;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -44,6 +49,30 @@ public class CalculatorApiControllerTest {
         ).andExpect(
                 MockMvcResultMatchers.content().string("60000")
         ).andDo(MockMvcResultHandlers.print());  // 내용 출력하기
+    }
+
+    @Test
+    public void minusTest() throws Exception {
+        // 우리가 request를 하나 보내야 하기 때문에 request 객체를 하나 만든다.
+        Req req = new Req();
+        req.setX(10);
+        req.setY(10);
+
+        String json = new ObjectMapper().writeValueAsString(req);   // req가 자동으로 json으로 바뀐다.
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("http://localhost:8080/api/minus")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        ).andExpect(
+                // 내가 기대하는 result의 value값은 0이다.
+                MockMvcResultMatchers.jsonPath("$.result").value("0")
+        ).andExpect(
+                // 내가 기대하는 response안에 있는 resultCode의 value값은 OK이다.
+                MockMvcResultMatchers.jsonPath("$.response.resultCode").value("OK")
+                ).andDo(MockMvcResultHandlers.print());
     }
 
 }
